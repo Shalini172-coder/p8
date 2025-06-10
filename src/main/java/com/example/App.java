48b90a05b886647eea772f54594eaaf2e089efca
Shalini172 <shalinib.22.becs@acharya.ac.in>
2025-06-10 04:26:37 +0000

    first commit:p8

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App 
+{
+
+public static void main(String[]args)
+    {
+    System.out.println("Application executed successfully!");
+    int a=1;
+    int b=3;
+    int result=a+b;
+    System.out.println("result is"+result);
+    }
+}
